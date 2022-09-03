728072d7ceaab68a2c7a699144d21d2faa4d3374
Gulshat <gubgg@yandex.ru>
2022-09-03 14:15:24 +0500

    Added search function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 const API_KEY = '22cd9319-2dd0-4d4d-b446-c98d6c5833f2';
 const API_URL_POPULAR = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1';
+const API_URL_SEARCH = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=';
+
+const form = document.querySelector("form");
+const search = document.querySelector(".header__search");
 
 getMovies(API_URL_POPULAR);
 
@@ -17,6 +21,8 @@ async function getMovies(url) {
 function showMovies(data) {
 	const moviesEl = document.querySelector(".movies");
 
+	document.querySelector(".movies").innerHTML = "";
+
 	data.films.forEach(movie => {
 		const movieEl = document.createElement("div");
 		movieEl.classList.add("movie");
@@ -28,11 +34,29 @@ function showMovies(data) {
 					</div>
 					<div class="movie__info">
 						<div class="movie__title">${movie.nameRu}</div>
-						<div class="movie__category">${movie.genres.map((genre) => `${genre.genre}`)}</div>
-						<div class="movie__average movie__average--green">8,3</div>
+						<div class="movie__category">${movie.genres.map((genre) => ` ${genre.genre}`)}</div>
+						<div class="movie__average movie__average--${showClassByRate(movie.rating)}">${movie.rating}</div>
 					</div>
 		`
-		// moviesEl.insertAdjacentHTML('beforeend', movieHTML);
 		moviesEl.appendChild(movieEl);
 	});
 }
+
+function showClassByRate(rating) {
+	if (rating >= 7) {
+		return "green"
+	} else if (rating >= 5) {
+		return "orange"
+	} else return "red"
+}
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	const apiSearchUrl = `${API_URL_SEARCH}${search.value}`;
+	if (search.value) {
+		getMovies(apiSearchUrl);
+
+		search.value = "";
+	}
+})
